65b5350b0dcac80b317ca2fde626774b47ba712e
e.tarasov <tarasovem@gmail.com>
2024-09-25 22:43:27 +0300

    Добавляет правила ES Lint и key для списка
    - Отключает proptypes
    - Отступы табами и форматирование файлов
    - Добавляет формирование key для списка на основе id

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,39 @@ import './App.css';
 import Button from './components/Button/Button';
 import JournalItem from './components/JournalItem/JournalItem';
 function App() {
-  const data = [
-    {
-      title: 'Подготовка к обновлению курсов',
-      date: new Date(),
-      text: 'Сегодня провёл весь день за...'
-    },
-    {
-      title: 'Поход в годы',
-      date: new Date(),
-      text: 'Думал, что очень много време...'
-    },
-    {
-      title: 'Подготовка к обновлению курсов',
-      date: new Date(),
-      text: 'Создал первую заметку, чтобы ...'
-    }
-  ];
+	const data = [
+		{
+			title: 'Подготовка к обновлению курсов',
+			date: new Date(),
+			text: 'Сегодня провёл весь день за...'
+		},
+		{
+			title: 'Поход в годы',
+			date: new Date(),
+			text: 'Думал, что очень много време...'
+		},
+		{
+			title: 'Подготовка к обновлению курсов',
+			date: new Date(),
+			text: 'Создал первую заметку, чтобы ...'
+		}
+	];
 
-  return (
-    <>
-      <h1>Заголовок</h1>
-      <p>Какой-то текст</p>
-      <Button/>
-      {data.map((item) => (
-        <JournalItem
-          key={item.id}
-          title={item.title}
-          date={item.date}
-          text={item.text}
-        />
-      ))}
-    </>
-  );
+	return (
+		<>
+			<h1>Заголовок</h1>
+			<p>Какой-то текст</p>
+			<Button/>
+			{data.map((item) => (
+				<JournalItem
+					key={item.id}
+					title={item.title}
+					date={item.date}
+					text={item.text}
+				/>
+			))}
+		</>
+	);
 }
 
 export default App;
--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,11 +1,11 @@
 import './JournalItem.css';
 
-function JournalItem({title, date, text}) {
+function JournalItem({title, date, text, id}) {
 	const formatter = new Intl.DateTimeFormat('ru-RU');
 	const FormattedDate = formatter.format(date);
-	
+
 	return (
-		<div className="journal-item" ke>
+		<div className="journal-item" key={id}>
 			<h2 className="journal-item__header">{title}</h2>
 			<div className="journal-item__body">
 				<div className="journal-item__date">{FormattedDate}</div>
