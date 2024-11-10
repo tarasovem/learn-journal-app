d29b63f0002d21d4348b6a4c9d60d7939f17536d
e.tarasov <tarasovem@gmail.com>
2024-11-10 18:49:53 +0300

    Добавляет использование localStorage для хранения списка

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,26 @@ import Header from './components/Header/Header';
 import JournalList from './components/JournalList/JournalList';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
-import { useState } from 'react';
-
-const INITIAL_DATA = [];
+import { useState, useEffect } from 'react';
 
 function App() {
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem('data'));
+		if (data) {
+			setItems(data.map((item) => ({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
+
+	useEffect(() => {
+		if (items.length) {
+			localStorage.setItem('data', JSON.stringify(items));
+		}
+	}, [items]);
 
 	const addItem = (item) => {
 		setItems(oldItems => [...oldItems, {
