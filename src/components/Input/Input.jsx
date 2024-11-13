80e8b4614bae6653d325c7986eb7880d54fdf502
e.tarasov <tarasovem@gmail.com>
2024-11-13 17:38:30 +0300

    Правки отображения Input
    - Убирает отладочную информацию
    - Убирает захардкоженный placeholder

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,20 +1,15 @@
 import styles from './Input.module.css';
 import cn from 'classnames';
-import { forwardRef, useEffect } from 'react';
+import { forwardRef } from 'react';
 
 const Input = forwardRef(({ className, isValid = true, appearance, ...props }, ref) => {
-
-	useEffect(() => {
-		console.log(appearance === 'title');
-	}, [appearance]);
-
 	return (
 		<input {...props} ref={ref} className={cn(className, styles['input'], {
 			[styles['invalid']]: !isValid,
 			[styles['title']]: appearance === 'title',
 			[styles['date']]: appearance === 'date',
 			[styles['tag']]: appearance === 'tag'
-		})} placeholder='Название' />
+		})} />
 	);
 });
 
